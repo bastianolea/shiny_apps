074f7d317eed48123b41a89dae5603d7846feb58
bastianolea <bastianolea@gmail.com>
2024-09-23 22:13:21 -0300

    aviso de clases

---
+++ colores.R
@@ -0,0 +1,68 @@
+
+color_fondo = "#1e0f26"
+color_texto = "#d3c7dd"
+color_enlaces = "#ba9bd7" #"#c8b1de"
+color_destacado = "#31193f"
+
+colores <- list()
+
+colores$relacionador <- list(
+  fondo = "#301519",
+  texto = "#fecea8",
+  borde = "#402A2D")
+
+colores$comparador <- list(
+  fondo = "#2a241d",
+  texto = "#cdcdb7",
+  borde = "#3C3631")
+
+colores$brechas <- list(
+  fondo = "#1c0e44",
+  texto = "#c1abfb",
+  borde = "#2D274A")
+
+colores$corrupcion <- list(
+  fondo = "#1d1e1d",
+  texto = "#8eccad",
+  borde = "#303130")
+
+colores$delincuencia <- list(
+  fondo = "#1f272b",
+  texto = "#cdf2ef",
+  borde = "#32393D")
+
+colores$millonarios <- list(
+  fondo = "#0e3c67",
+  texto = "#8BD8BD",
+  borde = "#2a5777")
+
+colores$censo <- list(
+  fondo = "#365c8d",
+  texto = "#abbbcf",
+  borde = "#3f6392")
+
+colores$femicidios <- list(
+  fondo = "#262626",
+  texto = "#f7d03a",
+  borde = "#4C4C4C")
+
+colores$cep <- list(
+  fondo = "#f4f8fe",
+  texto = "#4c79b6",
+  # borde = "#accdf9")
+  borde = "#BBBBBB")
+
+colores$comparador_mapas <- list(
+  fondo = "#181818",
+  texto = "#2AA198",
+  borde = "#252525")
+
+colores$economia <- list(
+  fondo = "#1C1833",
+  texto = "#CCBCFF",
+  borde = "#262145")
+
+colores$prensa <- list(
+  fondo = "#EEDABF",
+  texto = "#866C53",
+  borde = "#A5876A")
